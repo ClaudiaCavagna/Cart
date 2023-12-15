2c5e7c93d2296224a4c5f1636b80e5b63b42c907
ClaudiaCavagna <claudia.cavagna96@gmail.com>
2023-12-15 22:11:33 +0100

    style fixed

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { MdDelete } from 'react-icons/md';
+import { BiPlus, BiMinus } from 'react-icons/bi';
 
-const Item = () => {
+const Item = ({id, name, image, price, countInStock}) => {
   return (
-    <div>
-      
+    <div className='item'>
+      <img src={image} alt={name} className='prod-img'/>
+      <div className='prod-description'>
+        <h5>{name}</h5>
+        <hr/>
+        <p>{price}€</p>
+      </div>
+      <div className='prod-qty'>
+        <button>
+          <BiPlus />
+        </button>
+        <p>1</p>
+        <button>
+          <BiMinus />
+        </button>
+      </div>
+      <button>
+        <MdDelete />
+      </button>
     </div>
   )
 }
